f7313a5c073d0f59d22a3ce5e2559dcab24bb721
vidit kushwaha <viditkushwaha530@gmail.com>
2024-08-25 10:12:04 +0530

    Updated Config and resume pdf

--- config/index.js
+++ config/index.js
@@ -1,13 +1,20 @@
-import { instagram, twitter, linkedin, js, react, nodejs, mongodb, github } from "../src/assets";
+import {
+  instagram,
+  twitter,
+  linkedin,
+  js,
+  react,
+  nodejs,
+  mongodb,
+  github,
+} from '../src/assets'
 
-
-export const email = "viditkushwaha530@gmail.com";
-export const loaderDelay=2000;
-export const navDelay = 1000;
-export const resume = "/resume.pdf";
-export const collage = "https://www.google.com/search?q=nit+rourkela";
-export const repo = "https://github.com/Vidit-Kushwaha/pt";
-
+export const email = 'viditkushwaha530@gmail.com'
+export const loaderDelay = 2000
+export const navDelay = 1000
+export const resume = '/resume.pdf'
+export const collage = 'https://www.google.com/search?q=nit+rourkela'
+export const repo = 'https://github.com/Vidit-Kushwaha/pt'
 
 export const navLinks = [
   {
@@ -26,80 +33,97 @@ export const navLinks = [
 
 export const socialMedia = [
   {
-    id: "social-media-1",
+    id: 'social-media-1',
     icon: github,
-    link: "https://github.com/Vidit-Kushwaha",
+    link: 'https://github.com/Vidit-Kushwaha',
   },
   {
-    id: "social-media-2",
+    id: 'social-media-2',
     icon: instagram,
-    link: "https://www.instagram.com/vidit_kushwaha_/",
+    link: 'https://www.instagram.com/vidit_kushwaha_/',
   },
 
   {
-    id: "social-media-3",
+    id: 'social-media-3',
     icon: twitter,
-    link: "https://twitter.com/helloVidit",
+    link: 'https://twitter.com/helloVidit',
   },
   {
-    id: "social-media-4",
+    id: 'social-media-4',
     icon: linkedin,
-    link: "https://www.linkedin.com/in/vidit9011/",
+    link: 'https://www.linkedin.com/in/vidit9011/',
   },
-];
+]
 
 export const skills = [
   {
-    id: "js",
+    id: 'js',
     icon: js,
   },
   {
-    id: "react",
+    id: 'react',
     icon: react,
   },
   {
-    id: "nodejs",
+    id: 'nodejs',
     icon: nodejs,
   },
   {
-    id: "mongodb",
+    id: 'mongodb',
     icon: mongodb,
   },
   {
-    id: "github", 
+    id: 'github',
     icon: github,
   },
   {
-    id: "nextjs",
-    icon: "https://img.icons8.com/color/48/ffffff/nextjs.png",
-  }
-];
+    id: 'nextjs',
+    icon: 'https://img.icons8.com/color/48/ffffff/nextjs.png',
+  },
+]
 
 export const projects = [
   {
-    id: "inotebook",
-    title: "iNoteBook",
+    id: 'podwave',
+    title: 'Podwave - AI generated podcasts',
+    desc: 'Podwave is a web application that leverages AI-generated voices to create podcasts. Users can input text, select a voice, and generate a podcast episode with a single click. With a user-friendly interface and a variety of voice options, Podwave simplifies the process of creating engaging podcasts for content creators and listeners alike.',
+    external: 'https://podcast.viditkushwaha.com/',
+    stack: [
+      'GenAI',
+      'Open AI GPT 4.0',
+      'Google Text to Speech',
+      'Google Cloud',
+      'SSML',
+    ],
+  },
+  {
+    id: 'react-popupify',
+    title: 'React Popupify',
+    desc: 'React Popify is a React component library that provides a collection of customizable and responsive popups. With a variety of popup styles and animations, React Popify simplifies the process of creating engaging and interactive popups for web applications.',
+    external: 'https://www.npmjs.com/package/react-popupify',
+    stack: ['React', 'Rollup', 'SASS', 'npm'],
+  },
+  {
+    id: 'inotebook',
+    title: 'iNoteBook',
     desc: 'iNoteBook is a secure MERN stack app that simplifies note-taking. Users can create, organize, and manage their notes with ease through a user-friendly interface. With features like login authentication, tagging, and data encryption, iNoteBook ensures a seamless and secure note-taking experience.',
-    external: "https://yellow-hill-04ea5e410.2.azurestaticapps.net/",
+    external: 'https://yellow-hill-04ea5e410.2.azurestaticapps.net/',
     stack: ['MongoDb', 'Express', 'React', 'Node Js', 'Tailwind CSS'],
   },
   {
-    id: "news24/7",
-    title: "News 24/7",
+    id: 'news24/7',
+    title: 'News 24/7',
     desc: 'News 24/7 is a React-powered news website that leverages an external API to deliver up-to-date news content. With its user-friendly interface and seamless integration of the API, users can easily access and consume news articles on a variety of topics in real-time.',
-    github: "https://github.com/Vidit-Kushwaha/News-24-7",
+    github: 'https://github.com/Vidit-Kushwaha/News-24-7',
     stack: ['React', 'News API', 'Bootstrap'],
   },
   {
-    id: "pdf merger",
-    title: "PDF merger",
+    id: 'pdf merger',
+    title: 'PDF merger',
     desc: 'PDFMerger is a website built on Node.js and Express that utilizes an npm package to merge multiple PDF files seamlessly. With its user-friendly interface, users can easily upload their PDF files, and the application intelligently combines them into a single, consolidated PDF document, streamlining the process of merging PDFs effortlessly.',
-    github: "https://github.com/Vidit-Kushwaha/News-24-7",
+    github: 'https://github.com/Vidit-Kushwaha/News-24-7',
     stack: ['Node Js', 'Express Js', 'Bootstrap'],
   },
-];
-
-export const content = [
-  { },{ },{ },{ },{ },{ },{ },{ },{ },{ },{ },{ }, { }, { }
-  
 ]
+
+export const content = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}]
